efe90a96d7a8ad9411d373c9c19b6a77a0cc80d7
hung1962003 <106923246+hung1962003@users.noreply.github.com>
2025-06-07 16:30:34 +0700

    test

--- src/config/api.js
+++ src/config/api.js
@@ -1,11 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://35.232.143.173:8080/api",
   timeout: 5000,
   headers: {
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
+  transformRequest: [
+    (data) => {
+      return JSON.stringify(data);
+    },
+  ],
 });
 
 api.interceptors.request.use(
