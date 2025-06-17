6186bf320b56f253912bcdec9f435fe27c021eda
hung1962003 <106923246+hung1962003@users.noreply.github.com>
2025-06-17 16:12:44 +0700

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://34.96.206.25:8080/api", //
+  baseURL: "http://34.96.206.251:8080/api", //
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
