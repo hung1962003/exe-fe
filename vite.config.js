4f48b09d3d43f8cd2cec140b61ae7fc652fa7a99
hung1962003 <106923246+hung1962003@users.noreply.github.com>
2025-06-19 17:28:26 +0700

    deploytest1

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://34.96.206.251:8080', // backend chạy HTTP
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api'), // giữ nguyên /api
+      },
+    },
   },
 });
