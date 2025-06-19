d45cb03b1d9cbc9ecddf992cd8dcb24c5439e810
hung1962003 <106923246+hung1962003@users.noreply.github.com>
2025-06-19 18:29:26 +0700

    deploytest1

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: "/api", //
+   baseURL: "http://34.96.206.251:8080/api", //
   //baseURL: "http://34.96.206.251:8080/api", //
 
   timeout: 5000,
--- src/config/proxy.js
+++ src/config/proxy.js
@@ -1,18 +1,18 @@
-export default async function handler(req, res) {
-  const targetUrl = `http://34.96.206.251:8080${req.url.replace("/api", "")}`;
-  const response = await fetch(targetUrl, {
-    method: req.method,
-    headers: {
-      "Content-Type": "application/json",
-      ...(req.headers.authorization && {
-        authorization: req.headers.authorization,
-      }),
-    },
-    body: ["POST", "PUT", "PATCH"].includes(req.method)
-      ? JSON.stringify(req.body)
-      : undefined,
-  });
+// export default async function handler(req, res) {
+//   const targetUrl = `http://34.96.206.251:8080${req.url.replace("/api", "")}`;
+//   const response = await fetch(targetUrl, {
+//     method: req.method,
+//     headers: {
+//       "Content-Type": "application/json",
+//       ...(req.headers.authorization && {
+//         authorization: req.headers.authorization,
+//       }),
+//     },
+//     body: ["POST", "PUT", "PATCH"].includes(req.method)
+//       ? JSON.stringify(req.body)
+//       : undefined,
+//   });
 
-  const data = await response.json();
-  res.status(response.status).json(data);
-}
+//   const data = await response.json();
+//   res.status(response.status).json(data);
+// }
